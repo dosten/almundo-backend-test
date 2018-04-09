ea92f7e2187706a80aad217b8ef0d31b449fa58a
Diego Saint Esteben <diego@saintesteben.me>
2018-04-09 17:53:47 -0300

    Fix wrong comment

--- src/main/java/com/almundo/callcenter/employee/RoleComparator.java
+++ src/main/java/com/almundo/callcenter/employee/RoleComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import com.almundo.callcenter.employee.Employee;
 
 /**
- * This comparator compares employees by priority, ordering from low to high priority.
+ * This comparator compares employees by priority, ordering from high to low priority.
  */
 public class RoleComparator implements Comparator<Employee>
 {
